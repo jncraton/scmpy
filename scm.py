e96d7e4c5326ae9d4dc0cd650355b015af80ab1d
Jon Craton <jncraton@gmail.com>
2026-04-04 19:37:41 -0400

    Implement lambda

--- scm.py
+++ scm.py
@@ -1,3 +1,5 @@
+from functools import partial
+
 env = {
     '+': lambda a, b: a + b,
 }
@@ -9,9 +11,20 @@ def eval(sexp, env=env):
 
     >>> eval(['+', 1, ['+', 2, 2]])
     5
+
+    Anonymous `double` function
+    >>> eval([['lambda', ['n'], ['+', 'n', 'n']], 2])
+    4
     """
 
     if isinstance(sexp, int):
         return sexp
-    elif isinstance(sexp[0], str):
-        return env[sexp[0]](*map(eval, sexp[1:]))
+    if isinstance(sexp, str):
+        return env[sexp]
+    elif isinstance(sexp, list):
+        if sexp[0] == 'lambda':
+            names = sexp[1]
+            body = sexp[2]
+            return lambda *args: eval(body, env | {k:v for k,v in zip(names, args)})
+        else:
+            return eval(sexp[0], env)(*map(partial(eval, env=env), sexp[1:]))
