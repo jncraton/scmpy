35564556d7d285ba1bf25448eb250af3f676be43
Jon Craton <jncraton@gmail.com>
2026-04-04 16:55:28 -0400

    Implement addition

--- scm.py
+++ scm.py
@@ -1,6 +1,12 @@
-def eval(sexp):
+env = {
+    '+': lambda a, b: a + b,
+}
+
+def eval(sexp, env=env):
     """
     >>> eval(['+', 1, 1])
     2
     """
-    
+
+    if isinstance(sexp[0], str):
+        return env[sexp[0]](*sexp[1:])
