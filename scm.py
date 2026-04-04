824023bf64885a02479cab4c848efac11fb12601
Jon Craton <jncraton@gmail.com>
2026-04-04 16:58:36 -0400

    Support nested expressions

--- scm.py
+++ scm.py
@@ -6,7 +6,12 @@ def eval(sexp, env=env):
     """
     >>> eval(['+', 1, 1])
     2
+
+    >>> eval(['+', 1, ['+', 2, 2]])
+    5
     """
 
-    if isinstance(sexp[0], str):
-        return env[sexp[0]](*sexp[1:])
+    if isinstance(sexp, int):
+        return sexp
+    elif isinstance(sexp[0], str):
+        return env[sexp[0]](*map(eval, sexp[1:]))
