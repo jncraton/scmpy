c91d79c5242d887a4a53aef5d50ff022db0757ff
Jon Craton <jncraton@gmail.com>
2026-04-05 20:30:57 -0400

    Describe function

--- scm.py
+++ scm.py
@@ -1,8 +1,12 @@
 def eval(sexp, env=[{"+": lambda a, b: a + b}]):
     """
+    Evaluates `sexp` in `env`
+
     Examples
 
-    Function evaluation uses prefix notation
+    Function evaluation uses prefix notation.
+
+    `+` is provided as a function in the default global `env`
 
     >>> eval(['+', 1, 1])
     2
